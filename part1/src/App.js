42db517d7a3fb629a21cea3f7d0d51cd84b37f90
Zenrer <ram_idts@me.com>
2022-06-09 14:00:06 +1000

    finished part 1 exercise 2

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,9 +1,17 @@
 const Header = (props) => <h1>{props.course}</h1>;
 
-const Content = (props) => (
+const Part = (props) => (
   <p>
     {props.name} {props.exercises}
   </p>
+);
+
+const Content = (props) => (
+  <div>
+    <Part name={props.part1} exercises={props.exercises1} />
+    <Part name={props.part2} exercises={props.exercises2} />
+    <Part name={props.part3} exercises={props.exercises3} />
+  </div>
 );
 
 const Total = (props) => <p>Number of exercises {props.total}</p>;
@@ -20,9 +28,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content name={part1} exercises={exercises1} />
-      <Content name={part2} exercises={exercises2} />
-      <Content name={part3} exercises={exercises3} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
