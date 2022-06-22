b5c152ea0b54c7c3a7cdd402727cc7ab70b73379
Zenrer <ram_idts@me.com>
2022-06-22 16:20:22 +1000

    finished part 6 exercise 7

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,20 +1,11 @@
 import { useSelector, useDispatch } from "react-redux";
 
-import { newAnecdote, voteForAnecdote } from "./reducers/anecdoteReducer";
+import AnecdoteForm from "./components/AnecdoteForm";
+import { voteForAnecdote } from "./reducers/anecdoteReducer";
 
 const App = () => {
   const anecdotes = useSelector((state) => state);
   const dispatch = useDispatch();
-
-  const addAnecdote = (event) => {
-    event.preventDefault();
-
-    const content = event.target.content.value;
-
-    event.target.content.value = "";
-
-    dispatch(newAnecdote(content));
-  };
 
   const vote = (id) => {
     dispatch(voteForAnecdote(id));
@@ -34,13 +25,7 @@ const App = () => {
             </div>
           </div>
         ))}
-      <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div>
-          <input name="content" />
-        </div>
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteForm />
     </div>
   );
 };
