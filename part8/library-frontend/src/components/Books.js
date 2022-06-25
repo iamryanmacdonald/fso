6e0ad7ede35712d128380855323ca8235fe9d875
Zenrer <ram_idts@me.com>
2022-06-25 13:40:26 +1000

    finished part 8 exercise 19

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,15 +1,37 @@
 import { useQuery } from "@apollo/client";
+import { useState } from "react";
 
 import { ALL_BOOKS } from "../queries";
 
 const Books = (props) => {
+  const [genreFilter, setGenreFilter] = useState("all genres");
+
   const result = useQuery(ALL_BOOKS);
 
-  if (!props.show || result.loading) {
+  if (!props.show) {
     return null;
   }
 
+  if (result.loading) {
+    return <div>Loading...</div>;
+  }
+
   const books = result.data.allBooks;
+
+  const filteredBooks =
+    genreFilter === "all genres"
+      ? books
+      : books.filter((book) => book.genres.includes(genreFilter));
+
+  const genresObj = {};
+
+  books.forEach((book) =>
+    book.genres.forEach((genre) => (genresObj[genre] = true))
+  );
+
+  genresObj["all genres"] = true;
+
+  const genres = Object.keys(genresObj);
 
   return (
     <div>
@@ -22,7 +44,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {filteredBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -31,6 +53,10 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+
+      {genres.map((genre) => (
+        <button onClick={() => setGenreFilter(genre)}>{genre}</button>
+      ))}
     </div>
   );
 };
