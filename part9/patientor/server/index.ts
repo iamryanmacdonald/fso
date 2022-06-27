3ba31ae8178eb3227074aa719dedfd06df2323ed
Zenrer <ram_idts@me.com>
2022-06-27 15:01:59 +1000

    finished part 9 exercise 8 and exercise 9

---
+++ part9/patientor/server/index.ts
@@ -0,0 +1,17 @@
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/ping", (_req, res) => {
+  res.send("pong");
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
