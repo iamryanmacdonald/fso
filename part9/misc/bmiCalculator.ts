f3b0d0c9fae6bba907208961c1e9007fac3c34c5
Zenrer <ram_idts@me.com>
2022-06-27 11:43:39 +1000

    finished part 9 exercise 3

--- part9/misc/bmiCalculator.ts
+++ part9/misc/bmiCalculator.ts
@@ -1,3 +1,22 @@
+interface SafeBMIInputs {
+  height: number;
+  weight: number;
+}
+
+const parseBMIArguments = (args: Array<string>): SafeBMIInputs => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values were not numbers");
+  }
+};
+
 const calculateBmi = (height: number, weight: number): string => {
   const bmi: number = weight / Math.pow(height / 100, 2);
 
@@ -15,4 +34,13 @@ const calculateBmi = (height: number, weight: number): string => {
   return returnValue;
 };
 
-console.log(calculateBmi(180, 74));
+try {
+  const { height, weight } = parseBMIArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
--- part9/misc/exerciseCalculator.ts
+++ part9/misc/exerciseCalculator.ts
@@ -1,4 +1,9 @@
-interface result {
+interface SafeExerciseInputs {
+  target: number;
+  dailyHours: number[];
+}
+
+interface Result {
   periodLength: number;
   trainingDays: number;
   success: boolean;
@@ -8,12 +13,27 @@ interface result {
   average: number;
 }
 
-const calculateExercises = (dailyHours: number[]): result => {
+const parseExerciseArguments = (args: Array<string>): SafeExerciseInputs => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+
+  if (
+    !isNaN(Number(args[2])) &&
+    !args.slice(3).some((arg) => isNaN(Number(arg)))
+  ) {
+    return {
+      target: Number(args[2]),
+      dailyHours: args.slice(3).map((arg) => Number(arg)),
+    };
+  } else {
+    throw new Error("Provided values were not numbers");
+  }
+};
+
+const calculateExercises = (target: number, dailyHours: number[]): Result => {
   const totalHours = dailyHours.reduce((sum, hours) => (sum += hours), 0);
   const totalDays = dailyHours.length;
   const averageHours = totalHours / totalDays;
 
-  const target = 2;
   const targetHours = target * totalDays;
   let rating: 1 | 2 | 3 = 1;
   if (totalHours > 0.75 * targetHours) rating = 2;
@@ -43,4 +63,13 @@ const calculateExercises = (dailyHours: number[]): result => {
   };
 };
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1]));
+try {
+  const { target, dailyHours } = parseExerciseArguments(process.argv);
+  console.log(calculateExercises(target, dailyHours));
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
