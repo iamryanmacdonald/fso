5e0c7832fb64a21a0ede76cf924ef66a7cffd6f4
Zenrer <ram_idts@me.com>
2022-06-27 12:28:47 +1000

    finished part 9 exercise 5

--- part9/misc/index.ts
+++ part9/misc/index.ts
@@ -1,6 +1,36 @@
 import express from "express";
 
+import { calculateBmi } from "./bmiCalculator";
+
 const app = express();
+
+app.get("/bmi", (req, res) => {
+  const { height, weight } = req.query;
+
+  if (!height || !weight) {
+    res
+      .status(400)
+      .json({
+        error:
+          "Not enough arguments. Needs to include: height (cm), weight (kg)",
+      });
+  }
+
+  const heightNumber = Number(height);
+  const weightNumber = Number(weight);
+
+  if (isNaN(heightNumber) || isNaN(weightNumber)) {
+    res.status(400).json({ error: "malformatted parameters" });
+  }
+
+  const bmi: { height: number; weight: number; bmi: string } = {
+    height: heightNumber,
+    weight: weightNumber,
+    bmi: calculateBmi(heightNumber, weightNumber),
+  };
+
+  res.json(bmi);
+});
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
